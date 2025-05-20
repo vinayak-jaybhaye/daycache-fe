848f58f480ae97c7dcbf775c1db89cf32bc7fc5b
Vinayak Jaybhaye <vinayakjaybhaye795@gmail.com>
2025-05-20 18:19:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-900 h-screen flex flex-col items-center justify-center">
+      <div className="text-3xl font-bold mb-4">hello world</div>
+    </div>
+  );
+}
+
+export default App;
